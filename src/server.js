c9876e92ef21ae293161067905aeba18b4193b8c
Pusztai, Ryan <ryan.pusztai@gentex.com>
2019-11-27 11:40:26 -0500

    Configured the site to run the Netlify CMS

--- src/server.js
+++ src/server.js
@@ -5,13 +5,14 @@ import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
+console.log( "[PORT]", PORT );
 
 polka() // You can also use Express
 	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
+		compression( { threshold: 0 } ),
+		sirv( 'static', { dev } ),
 		sapper.middleware()
 	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+	.listen( PORT, err => {
+		if ( err ) console.log( 'error', err );
+	} );
